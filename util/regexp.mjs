ee0d1cf5d5884c87cdc62e9e674db51699ab7a74
Yuchen Liu <liuyuchenzh@163.com>
2020-03-02 14:03:25 +0800

    fix: use more strict regExp to match chunk loading

--- util/regexp.mjs
+++ util/regexp.mjs
@@ -1,7 +1,7 @@
 export const getPublicPathExp = () =>
   /__webpack_require__\.p\s?=\s?([^;\n]+);?/g
 export const getV2ScriptRegExp = () =>
-  /__webpack_require__\.p\s?\+[\s\S]*?(chunkId)[\s\S]*?\.js['"];?/g
+  /__webpack_require__\.p\s?\+[^\n]*?(chunkId)[^\n]*?\.js['"];?/g
 export const getScriptRegExp = () =>
   /__webpack_require__\.p\s?\+[^[]+\[(\S+)][^\n]+?\.js['"];?/g
 export const getCssChunksRegExp = () => /var\scssChunks\s*=\s*([^;\n]+);?/
